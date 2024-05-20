e9768697baeb38d5bd71460bd6a24e3970d89c41
zypherson <taylorzypherson@gmail.com>
2024-05-20 16:56:04 -0400

    updated proj

--- api/server.js
+++ api/server.js
@@ -1,2 +1,16 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('*', (req,res)=>{
+    res.status(404).json({
+        message:'not found babe'
+    })
+})
+
+
+module.exports = server
