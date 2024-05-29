cb1f69453b2a5dfa0b5d3d096f72c498682cfd7a
zypherson <taylorzypherson@gmail.com>
2024-05-29 12:51:44 -0400

    updated mvp

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -30,12 +30,31 @@ router.get('/:id', async (req,res) =>{
              }
     }catch (err){
         res.status(500).json({
-            message:'The posts information could not be retrieved'
+            message:'The post with the specified ID does not exist'
         })
     }
 })
 router.post('/', (req,res) =>{
-
+    const { title, contents} = req.body
+    if(!title || !contents) {
+        res.status(400).json({
+            message:'Please provide title and contents for the post'
+        })
+    }else{
+       Post.insert({ title, contents})
+        .then(({id}) =>{
+            return Post.findById(id)
+        })
+        .then(post =>{
+            res.status(201).json(post)
+        })
+        
+        .catch(err=>{
+            res.status(500).json({
+                message:'There was an error while saving the post to database'
+            })
+        })
+    }
 })
 router.delete('/:id', (req,res) =>{
 
